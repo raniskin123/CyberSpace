575828c559c5607f670705871a4290d408bae3e0
Ranis <hametsinr98@gmail.com>
2025-05-04 21:37:32 +0300

    первый коммит

---
+++ javascript/main.js
@@ -0,0 +1,5 @@
+let burgerIcon = document.querySelector(".burger_menu");
+let burger = document.querySelector(".burger");
+burgerIcon.addEventListener("click", ()=>{
+    burger.classList.toggle("active")
+})
